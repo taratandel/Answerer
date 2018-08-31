365e69181ea81c21389570b803d2bfbb76983a3d
Tara Tandel <tara.tandel@icloud.com>
2018-09-01 01:00:44 +0430

    getting Chats Every

---
+++ Answerer/Cells/ChatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatTableViewCell.swift
+//  Answerer
+//
+//  Created by Tara Tandel on 6/2/1397 AP.
+//  Copyright © 1397 negar. All rights reserved.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
